232ff32bd8207f074c8cea9f44708690a1f278c5
Edwiler de Lima Araujo <edwillerdelima@gmail.com>
2026-05-22 20:30:51 -0500

    adiicionando js - REF #12

---
+++ javascript´s/js.js
@@ -0,0 +1,43 @@
+console.log("Hello World JavaScript")
+
+console.error("Esse é um erro")
+
+console.warn("Esse é um aviso")
+
+var x = 10
+
+let y = 15
+
+const z = 20
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+const nome = "edwiller de lima araujo"
+
+console.log(nome)
+
+const shirtQty = 4
+
+console.log(shirtQty)
+
+console.log(typeof(shirtQty))
+
+
+const isApproved = false 
+
+console.log(typeof(isApproved))
+
+let surname = null
+
+console.log(typeof(surname))
+
+let language = ["Javascript,PHP,PYthon"]
+console.log(language)
+console.log(typeof(language))
+
+
+const fullname = "Edwille de Lima Araujo"
+
+console.log(fullname.length)
